0189dafb2fb5b84ed8d439dcf40db379102192c4
Tarcisio Câmara <tarcisiodantasrn@gmail.com>
2020-02-20 12:11:36 +0000

    Opa

--- __tests__/Hello.test.js
+++ __tests__/Hello.test.js
@@ -8,4 +8,9 @@ test("sayHello()", () => {
 test("sum()", () => {
 	let p = new Hello();
 	expect(p.sum(5, 7)).toBe(12);
+});
+
+test("mult()", ()=> {
+    let p = new Hello();
+    expect(p.mult(5, 10)).toBe(50);
 })
--- src/Hello.js
+++ src/Hello.js
@@ -1,8 +1,14 @@
+import TestRunner from "jest-runner";
+
 export default class Hello {
 	sum(x, y) {
 		return x + y;
 	}
 	sayHello() {
 		return "hello"
-	}
+    }
+    mult(x, y){
+        return x*y
+    }
 }
+
